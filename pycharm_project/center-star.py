48ff2daf75e05fac3f0b6f29c58197929fb87809
Timo Wentong Lin <lin@uni-bielefeld.de>
2018-01-21 18:15:35 +0100

    implement multiple alignment, last step hirschberg missing

--- pycharm_project/center-star.py
+++ pycharm_project/center-star.py
@@ -17,7 +17,7 @@ def main():
 
 # calculates the edit distance for two DNA sequences and a
 # score matrix, if no cost matrix is given, unit costs will be used
-def edit_distance(seq1, seq2, mat=None):
+def edit_distance(seq1, seq2, mat=None, indel_cost = 1):
     if mat is None:
         mat = UNIT_COST_MATRIX
     # initialize the first row of matrix
@@ -32,7 +32,7 @@ def edit_distance(seq1, seq2, mat=None):
         for j in range(len(seq1)):
             j = j + 1
             # implements the recursive definition, view sequence analysis script
-            row2[j] = max(row1[j - 1] + get_score(seq1[j - 1], seq2[i - 1], mat), row1[j] + 1, row2[j - 1] + 1)
+            row2[j] = max(row1[j - 1] + get_score(seq1[j - 1], seq2[i - 1], mat), row1[j] + indel_cost, row2[j - 1] + indel_cost)
         row1 = row2
     return row1[len(row1) - 1]
 
@@ -44,14 +44,71 @@ def get_score(base1, base2, mat):
     return mat[index1][index2]
 
 
-def add_sequence_to_alignment(alignment, seq):
+def get_score_alignment(alignmentColumn, base, mat):
+    totalScore = 0
+    alignmentColumn += [base]
+    for i in range(len(alignmentColumn)):
+        for j in alignmentColumn:
+            totalScore += get_score(j, base, mat)
+        alignmentColumn.remove(alignmentColumn[0])
+    return totalScore
+
+
+def calculate_alignment(center_star, sequences, mat):
+    multiple_alignment = [c for c in center_star]
+    for i in range(len(sequences)):
+        append_next_sequence(multiple_alignment, sequences[i], mat)
+    return multiple_alignment
+
+
+def append_next_sequence(multiple_alignment, seq, mat):
+    new_multiple_alignment = [multiple_alignment]
+    while True:
+        coordinates = get_next_iteration_hirschberg(multiple_alignment, seq, mat)
+
+
+def get_next_iteration_hirschberg(alignment, seq, mat):
     row1 = [i for i in range(len(alignment) + 1)]
     row2 = [len(alignment) + 1]
-    hirschbergRowOne = get_hirschberg(row1, row2, alignment, seq)
-    hirschbergRowTwo = get_hirschberg(row1.reverse(), row2, alignment.reverse(), seq.reverse())
+    hirschberg_row_one = get_hirschberg(row1, row2, alignment, seq, mat)
+    row1 = [i for i in range(len(alignment) + 1)]
+    row2 = [len(alignment) + 1]
+    hirschberg_row_two = get_hirschberg(row1.reverse(), row2, alignment.reverse(), seq.reverse(), mat)
+    row1 = [i for i in range(len(alignment) + 1)]
+    row2 = [len(alignment) + 1]
+    edit_dist = get_edit_distance_multiple(row1, row2, alignment, seq, mat)
+    i_coordinate = 0
+    j_coordinate = (len(seq)+1)/2 + ((len(seq) + 1) % 2)
+    for i in range(len(hirschberg_row_one)):
+        if (hirschberg_row_one[i] + hirschberg_row_two[i]) - edit_dist == 0:
+            i_coordinate = i
+            break
+    return i_coordinate, j_coordinate
 
-def get_hirschberg(row1, row2, alignment, seq):
-    limit = len()
+
+def get_edit_distance_multiple(row1, row2, alignment, seq, mat):
+    for i in range(seq):
+        i = i + 1
+        row2[0] = i
+        for j in range(len(alignment)):
+            j = j + 1
+            # implements the recursive definition, view sequence analysis script
+            row2[j] = max(row1[j - 1] + get_score_alignment(alignment[j - 1], seq[i - 1], mat), row1[j] + 1, row2[j - 1] + 1)
+        row1 = row2
+        return row1[len(row1)-1]
+
+
+def get_hirschberg(row1, row2, alignment, seq, mat):
+    limit = int((len(seq)+1)/2) + ((len(seq) + 1) % 2)
+    for i in range(limit-1):
+        i = i + 1
+        row2[0] = i
+        for j in range(len(alignment)):
+            j = j + 1
+            # implements the recursive definition, view sequence analysis script
+            row2[j] = max(row1[j - 1] + get_score_alignment(alignment[j - 1], seq[i - 1], mat), row1[j] + 1, row2[j - 1] + 1)
+        row1 = row2
+    return row1
 
 
 if __name__ == '__main__':
